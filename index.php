ea5d8ad7dca59396af13f669dadad418405ee066
Breno Vambaster <brenovambaster5@gmail.com>
2020-01-28 00:33:44 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,216 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="cover.css">
+    <link rel="stylesheet" href="carousel.css">
+    <title>Imantel Engenharia</title>
+</head>
+
+<body>
+
+    <div>
+        <nav class="menuedit navbar navbar-expand-lg navbar-dark bg-dark " style="font-size:17px;">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="index.php"><img src="imgs/logo3.png" alt="logo" width="250px" height="100px"></a>
+
+            <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
+
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0 mx-auto nav-masthead cor-laranja">
+                    <li class="nav-item ">
+                        <a class="nav-link text-light" href="#empresa">Empresa </a>
+                    </li>
+
+                    <li class="nav-item ">
+                        <a class="nav-link text-light  " href="#vantagens">Vantagens </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light " href="#"> Serviços</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light  " href="#"> Projetos </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light  " href="#"> Clientes </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light  " href="#"> Contato </a>
+                    </li>
+
+
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+    <section class="bg-dark section1">
+        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <main role="main" class="inner cover" style="color: #FFF">
+                <h1 class="cover-heading">Bem Vindo à Imantel Engenharia!</h1>
+                <p class="lead">Aqui você encontra qualificação em mão de obra. Profissionais capacitados para atender na melhor forma a sua necessidade. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                <p class="lead">
+                    <a href="#" class="btn btn-md btn-secondary">Learn more</a>
+                </p>
+            </main>
+
+        </div>
+    </section>
+    <section class="container" id="empresa">
+        <div class="empresa_title">
+            <h2 class="text-center fonte_h1"> Imantel <br> Soluções em Engenharia</h2>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-md-6 text-justify float-left">
+                <p>Fundada em 2005, a Conection se apresenta ao mercado como empresa de engenharia elétrica especializada em montagens elétricas para
+                    diferentes segmentos, com destaque para subestação, mineração, siderurgia e cimento. Ao longo dos anos consolidou-se nos
+                    ramos de engenharia elétrica detalhada, treinamentos, consultoria, fiscalização e supervisão das montagens elétricas.</p>
+            </div>
+            <div class="col-md-6">
+                <div id="carouselExampleCaptions" class="carousel slide rounded-lg rounded-sm" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="imgs/img3.jpg" class="d-block w-100" alt="img3">
+                            <div class="carousel-caption d-none d-sm-block">
+                                <h5>Projeto de automação comercial</h5>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imgs/img1.jpg" class="d-block w-100 h-200" alt="img4">
+                            <div class="carousel-caption d-none d-sm-block">
+                                <h5>Second slide label</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imgs/img2.jpg" class="d-block w-100" alt="img2">
+                            <div class="carousel-caption d-none d-sm-block">
+                                <h5>Third slide label</h5>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+    <section class="vantages" id="vantagens">
+
+        <div class="row">
+            <div class="col-md-7 text-center offset-md-3">
+                <h1 class="cor-vantagem fonte_h1 mt-5">Vantagens Imantel</h1>
+                <p class=" lead text-preto text-justify">Comportamento e interação nas atividades da empresa fundamentada em Segurança, Experiência,
+                    Integridade e Qualidade, maximizando os resultados com investimento em material humano.
+                    É movida pelo desafio constante de apresentar soluções completas para os empreendimentos.
+                </p>
+            </div>
+        </div>
+        <div class="container marketing">
+
+            <div class="row featurette">
+
+                <div class="col-md-7">
+                    <h2 class="featurette-heading cor-vantagem"> <strong class="text-danger"> 01.</strong> <br> Segurança</h2>
+                    <p class="lead text-preto ">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                </div>
+                <div class="col-md-5">
+                    <img src="" alt="">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+
+                <div class="col-md-7 order-md-1">
+                    <h2 class="featurette-heading cor-vantagem"><strong class="text-primary"> 02.</strong> <br> Experiência de Mercado</h2>
+                    <p class="lead text-preto ">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                </div>
+                <div class="col-md-5">
+                    <img src="" alt="">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+
+                <div class="col-md-7 ">
+                    <h2 class="featurette-heading cor-vantagem"><strong class="text-success"> 03.</strong> <br> Integridade</h2>
+                    <p class="lead text-preto ">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                </div>
+                <div class="col-md-5">
+                    <img src="" alt="">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <!-- /END THE FEATURETTES -->
+
+        </div><!-- /.container -->
+    </section>
+
+    <section class="section-3">
+        <div class="container marketing mt-5">
+            <h1 class="text-center fonte_h1 mt-5">Nossos clientes</h1>
+            <!-- Three columns of text below the carousel -->
+            <div class="row mb-5">
+                <div class="col-lg-4">
+                    <img src="imgs/jack.jpg" alt="" width="160" height="160" background="#777" color="#777" class="rounded-circle">
+                    <h2>Heading</h2>
+                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img src="imgs/rodrigo.jpg" alt="" width="160" height="160" background="#777" color="#777" class="rounded-circle">
+                    <h2>Heading</h2>
+                    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img src="imgs/anonimo.jpg" alt="" width="160" height="160" background="#777" color="#777" class="rounded-circle">
+                    <h2>Heading</h2>
+                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
+
+        </div>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
